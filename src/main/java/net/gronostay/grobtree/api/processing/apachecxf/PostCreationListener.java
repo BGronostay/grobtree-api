0da611c17752c325479a79f7e804eff17f372b33
Bernd Gronostay <32064019+BGronostay@users.noreply.github.com>
2024-12-22 05:02:55 +0100

    PostCreationListener -> map

--- src/main/java/net/gronostay/grobtree/api/processing/apachecxf/PostCreationListener.java
+++ src/main/java/net/gronostay/grobtree/api/processing/apachecxf/PostCreationListener.java
@@ -3,6 +3,11 @@ package net.gronostay.grobtree.api.processing.apachecxf;
 import net.gronostay.grobtree.api.nodes.SimpleLogNode;
 import net.gronostay.grobtree.api.nodes.TopLogNode;
 
+import java.util.Map;
+
 public interface PostCreationListener {
-    void onPayloadNodeCreated(TopLogNode topNode, String serviceName, SimpleLogNode payloadNode, String payload);
+    @Deprecated
+    default void onPayloadNodeCreated(TopLogNode topNode, String serviceName, SimpleLogNode payloadNode, String payload) {}
+
+    default void onPayloadNodeCreated(TopLogNode topNode, Map<String, String> valuesMap, SimpleLogNode payloadNode, String payload) {}
 }
