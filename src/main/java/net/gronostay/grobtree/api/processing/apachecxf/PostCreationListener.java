00e988bd3ffbcb2390a94614e4b2f37dd5bc72ab
Bernd Gronostay <32064019+BGronostay@users.noreply.github.com>
2025-05-14 00:09:00 +0200

    CXF PostCreationListener Add onNoNodeCreated

--- src/main/java/net/gronostay/grobtree/api/processing/apachecxf/PostCreationListener.java
+++ src/main/java/net/gronostay/grobtree/api/processing/apachecxf/PostCreationListener.java
@@ -1,7 +1,10 @@
 package net.gronostay.grobtree.api.processing.apachecxf;
 
+import net.gronostay.grobtree.api.global.Local;
 import net.gronostay.grobtree.api.nodes.SimpleLogNode;
 import net.gronostay.grobtree.api.nodes.TopLogNode;
+import net.gronostay.grobtree.api.processing.LogEntry;
+import net.gronostay.grobtree.api.processing.LogTree;
 
 import java.util.Map;
 
@@ -10,4 +13,6 @@ public interface PostCreationListener {
     default void onPayloadNodeCreated(TopLogNode topNode, String serviceName, SimpleLogNode payloadNode, String payload) {}
 
     default void onPayloadNodeCreated(TopLogNode topNode, Map<String, String> valuesMap, SimpleLogNode payloadNode, String payload) {}
+
+    default void onNoNodeCreated(LogEntry logEntry, LogTree logTree, Local local) {}
 }
